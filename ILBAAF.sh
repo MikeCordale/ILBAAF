472f9a548d375b37a8731300b3ac8a50a24cccb0
MikeCordale <73846431+MikeCordale@users.noreply.github.com>
2021-03-04 02:34:52 +0400

    Update ILBAAF.sh

--- ILBAAF.sh
+++ ILBAAF.sh
@@ -1,2 +1,2 @@
-defps1="PS1='\\\[\\\e[39m\\\]\\\[\\\e[33m\\\]I Love BDSM And Anal Fisting | \\\[\\\e[0m\\\]\\\[\\\e[0m\\\]\\\[\\\e[96m\\\]'"
+defps1="PS1='\\\[\\\e[39m\\\]\\\[\\\e[33m\\\]FROM THE HELL | \\\[\\\e[0m\\\]\\\[\\\e[0m\\\]\\\[\\\e[96m\\\]'"
 sed -i "s/.*PS1=.*/$defps1/" /data/data/com.termux/files/usr/etc/bash.bashrc
