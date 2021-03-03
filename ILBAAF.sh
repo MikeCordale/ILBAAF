e8709519c5ec233fe38eec0d1293d003b036d9fc
MikeCordale <73846431+MikeCordale@users.noreply.github.com>
2021-03-04 02:36:29 +0400

    Update and rename ILBAAF.sh to Hell

--- ILBAAF.sh
+++
@@ -1,2 +0,0 @@
-defps1="PS1='\\\[\\\e[39m\\\]\\\[\\\e[33m\\\]I Love BDSM And Anal Fisting | \\\[\\\e[0m\\\]\\\[\\\e[0m\\\]\\\[\\\e[96m\\\]'"
-sed -i "s/.*PS1=.*/$defps1/" /data/data/com.termux/files/usr/etc/bash.bashrc
